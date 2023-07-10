910bc3ea9be9b9929a5fef996a560170a2a80e0d
shefroyrowe <shefroy.rowe@gmail.com>
2023-07-10 13:55:59 -0500

    Ui complete > btn images added > winner/loser display area  updated

--- scripts.js
+++ scripts.js
@@ -2,10 +2,19 @@
 let draws = 0;
 let computerScore = 0;
 let playerScore = 0;
+
 //reference html display area for results
 let display = document.querySelector('.display');
-let intro = "Get ready! five rounds! -ROCK- -PAPER- -SCISSORS- Lets go!!!"
-alert(intro)
+//reference html tooltip text area for responses
+let tooltip = document.querySelector('.tooltip');
+//reference html win-lose announcement element
+let announcement = document.querySelector('.win-lose');
+
+//introduction 
+
+let intro = "GET READY! 5 ROUNDS! -ROCK- -PAPER- -SCISSORS- LETS GO!!!";
+alert(intro);
+
 
  //user button controls
     let buttons = document.querySelectorAll('button');
@@ -15,11 +24,11 @@ alert(intro)
 
 //game logic container
 function game(){ 
-   
-    let playerOp = this.id;//get user response
-    alert(`player: "${playerOp}"`); //alert user response
-    
-    let computerOp = Math.floor(Math.random() * 3);//get computer response
+   //get user response
+    let playerOp = this.id;
+     
+    //get computer response
+    let computerOp = Math.floor(Math.random() * 3);
     if (computerOp === 0) {
         computerOp = 'rock';
     } else if (computerOp === 1) {
@@ -27,9 +36,13 @@ function game(){
     } else {
         computerOp = 'scissors';
     }
-    alert(`computer: "${computerOp}"`);//alert computer response
+    
+    //display player and computer response
+    tooltip.textContent = `PLAYER: "${playerOp}"\n\nCOMPUTER: "${computerOp}"`;
+    //reset announcement css visibility value to hidden
+    announcement.style.cssText = 'visibility: hidden';
    
-        //game logic
+        //gameplay logic
         if (computerOp === playerOp) { //if both players pick the same value
             draws++;
             display.textContent = 'It\'s a draw!';
@@ -46,22 +59,32 @@ function game(){
             playerScore++;
             display.textContent = `${playerOp} beats ${computerOp}, player: ${playerScore} points`;//update display
         }
-        if((draws + computerScore + playerScore) == 5){
-            display.textContent = `You've won ${playerScore} times, drawn ${draws} times and lost ${computerScore} times.`
+
+        //Winner anouncement and stats
+        if((draws + computerScore + playerScore) == 5 && playerScore > computerScore){
+            display.textContent = `Wins ${playerScore}, draws ${draws}, losses ${computerScore}`;
+            //set announcement text content and css visibily value for win
+            announcement.textContent = 'WINNER!!';
+            announcement.style.cssText = 'visibility: visible';
 
             playerScore = 0;
             computerScore = 0;
             draws = 0;
+            return; //break and restart if...else
+
+        }else if((draws + computerScore + playerScore) == 5 && playerScore < computerScore){
+            display.textContent = `You Won ${playerScore}, had ${draws} draws, and ${computerScore} losses`;
+            //set announcement text content and css value visibily for loss
+            announcement.textContent = 'YOU LOSE';
+            announcement.style.cssText = 'visibility: visible';
+
+            playerScore = 0;
+            computerScore = 0;
+            draws = 0;
+            return; //break and restart if...else
         }
-    }
-
-    /*
-      add rock, paper and scissor images respectively
-      switch info from alerts over to tooltip div
-
-      add computer wins or players wins to tooltip dive after five rounds
-    */
-    
+    } 
+   
 
 
    
