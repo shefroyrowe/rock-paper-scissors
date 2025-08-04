5700f9c7de8ca2910c31301b00d59e9ad6954c39
unknown <shefroy.rowe@gmail.com>
2025-08-04 00:31:42 -0500

    add playround logic

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
+const playerScoreDisplay = document.getElementById('playerScore');
+const computerScoreDisplay = document.getElementById('computerScore');
+
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3);
+    const choice = Math.floor(Math.random() * 3);
+    if (choice === 0) {
+        return 'rock';
+    } else if (choice === 1) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
 }
 
 function getHumanChoice() {
     const buttons = document.querySelectorAll('.game-btn');
     buttons.forEach(btn => btn.addEventListener('click', (e) => {
-        console.log(btn.id);
         return btn.id;
     }));
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log('it\'s a draw');
+    } else if (humanChoice === 'rock'
+        && computerChoice === 'scissors'
+        || humanChoice === 'paper'
+        && computerChoice === 'rock'
+        || humanChoice === 'scissors'
+        && computerChoice === 'paper') {
+        console.log(`You win ${humanChoice} beats ${computerChoice}`);
+    } else {
+        console.log(`Computer wins ${computerChoice} beats ${humanChoice}`);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
