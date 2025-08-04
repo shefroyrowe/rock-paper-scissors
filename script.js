fb1f41888c372dc0a24d2e3b596ae61e9e39e664
unknown <shefroy.rowe@gmail.com>
2025-08-04 18:08:59 -0500

    add endgame logic and reset logic

--- script.js
+++ script.js
@@ -13,20 +13,44 @@ let computerScore = 0;
 
 function playRound(humanChoice) {
     const choices = ['rock', 'paper', 'scissors'];
-  const computerChoice = choices[Math.floor(Math.random() * choices.length)];
- 
-  // Determine the winner and update the resultDisplay with the outcome
-  if (humanChoice === computerChoice) {
-      messageDisplay.textContent = 'It\'s a draw!';
-  } else if (
-      (humanChoice === 'rock' && computerChoice === 'scissors') ||
-      (humanChoice === 'paper' && computerChoice === 'rock') ||
-      (humanChoice === 'scissors' && computerChoice === 'paper')
-  ) {
-      messageDisplay.textContent = 'You win!\n' + humanChoice + ' beats ' + computerChoice;
-  } else {
-      messageDisplay.textContent = 'Computer wins!\n' + computerChoice + ' beats ' + humanChoice;
-  }
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+    // Determine the winner and update the resultDisplay with the outcome
+    if (humanChoice === computerChoice) {
+        messageDisplay.textContent = 'It\'s a draw!';
+        rounds++;
+        displayRound.textContent = `${rounds} 0f 5`;
+    } else if (
+        (humanChoice === 'rock' && computerChoice === 'scissors') ||
+        (humanChoice === 'paper' && computerChoice === 'rock') ||
+        (humanChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        messageDisplay.textContent = 'You win!\n' + humanChoice + ' beats ' + computerChoice;
+        humanScore++;
+        rounds++;
+        displayRound.textContent = `${rounds} 0f 5`;
+    } else {
+        messageDisplay.textContent = 'Computer wins!\n' + computerChoice + ' beats ' + humanChoice;
+        computerScore++;
+        rounds++;
+        displayRound.textContent = `${rounds} 0f 5`;
+    }
+
+    // Update the score display
+    playerScoreDisplay.textContent = humanScore;
+    computerScoreDisplay.textContent = computerScore;
+
+    if (rounds === 5) {
+        if (humanScore === computerScore) return messageDisplay.textContent = 'Game is a tie! [Press any key to continue]';
+        messageDisplay.textContent = `${humanScore > computerScore
+            ? 'Player wins all! [Press any key to continue]'
+            : 'Computer takes the game! [Press any key to continue]'}`;
+
+        // reset
+        rounds = 0;
+        humanScore = 0
+        computerScore = 0;
+    }
 }
 
 rockBtn.addEventListener('click', () => playRound('rock'));
